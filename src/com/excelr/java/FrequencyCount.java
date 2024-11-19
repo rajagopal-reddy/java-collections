804ec56f24567d04298d892b6bfdc1d9344973cf
rajagopal-reddy <rajagopalreddy337@gmail.com>
2024-11-19 15:44:15 +0530

    chore: frequency count with hashmap

---
+++ src/com/excelr/java/FrequencyCount.java
@@ -0,0 +1,23 @@
+package com.excelr.java;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String items = scanner.nextLine();
+        HashMap<String, Integer> fruits = new HashMap<>();
+
+        for(String fruit : items.trim().split(" ")){
+            fruits.put(fruit,fruits.getOrDefault(fruit,0)+1);
+        }
+
+        System.out.println(fruits);
+
+        for (var value: fruits.entrySet()) {
+            System.out.println(value.getKey() + " = " + value.getValue());
+        }
+    }
+}
